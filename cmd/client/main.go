e2253c8400013efe662e4db7dc995c62165f1a5e
Ryan Showalter <showalrm@dukes.jmu.edu>
2021-03-24 18:28:23 -0400

    Add commenting and HTTP response

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,30 +1,42 @@
+// This file contains the main routine for clients.
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"os"
 	"fmt"
 )
 
+// The entry point of the program
 func main() {
 
+	// The command line arguments. args[0] is the name of the program.
 	args := os.Args
 
+	// If there was no argument passed, ask for one and exit.
 	if len(args) == 1 {
 		fmt.Println("Please pass a file name to send to the server.")
 		os.Exit(1)
 	}
 
+	// Open the file whose name was passed as an argument.
 	dat, err := os.Open(args[1])
 	if err != nil {
 		panic(err)
 	}
 
-	_, err = http.Post("http://127.0.0.1:39485/newjob", "text/plain", dat)
+	// Send a post request to the worker.
+	resp, err := http.Post("http://127.0.0.1:39485/newjob", "text/plain", dat)
 	if err != nil {
 		panic(err)
 	}
+	
+	// Put the bytes from the request into a file
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	file := buf.String()
 
-	fmt.Println("Done?")
+	fmt.Println(file)
 
 }
