7169769d9accc2cdf3af56c4f1b3ef8fc14124a9
z-intercepts <zachptucker@gmail.com>
2021-04-20 12:39:57 -0400

    add ability to run binary files

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -7,6 +7,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -26,27 +28,28 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Open the file whose name was passed as an argument.
-	code, err := ioutil.ReadFile(args[2])
-	if err != nil {
-		fmt.Println("Error opening file. Aborting")
-		os.Exit(3)
-	}
-
 	// Get extension and file name
-	fullPath := strings.Split(args[2], "/")
-	fileName := fullPath[len(fullPath)-1]
-	extension := ""
-	if strings.Contains(args[2], ".") {
-		extension = strings.Split(args[2], ".")[1]
+	fileName, extension := getFileName(args[2])
+
+	// Code is unessesary to send if executable exists
+	var code []byte
+	if extension == "executable" {
+		code = nil
 	} else {
-		extension = "none"
+		// File is not an executable, so copy code
+		// Open the file whose name was passed as an argument.
+		var err error
+		code, err = ioutil.ReadFile(args[2])
+		if err != nil {
+			fmt.Println("Error opening file. Aborting")
+			os.Exit(3)
+		}
 	}
 
 	// Make a job with the given code.
 	jobBytes := data.JobDataToJson(1, time.Now(), 2, 1, 10, fileName, extension, code)
 
-	// Send a post request to the worker.
+	// Send a post request to the supervisor.
 	resp, err := http.Post(args[1]+"/newjob",
 		"text/plain", bytes.NewReader(jobBytes))
 	if err != nil {
@@ -61,4 +64,55 @@ func main() {
 
 	fmt.Println(file)
 
+}
+
+/* ----- Helper functions ----- */
+
+// Check for an error.
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+// Find the absolute path of a file
+func findAbsolute(fileName string) string {
+	var out string
+	var err error
+
+	// First check if file is a binary
+	out, err = exec.LookPath(fileName)
+
+	// If file is not a binary, try to find abs path
+	if err != nil {
+		out, err = filepath.Abs(fileName)
+		check(err)
+	}
+
+	// If no error return absolute path
+	return out
+}
+
+// Get the filename and extension type of a file
+func getFileName(arg string) (string, string) {
+	abs := findAbsolute(arg)
+
+	// Get file name
+	fullPath := strings.Split(abs, "/")
+	fileName := fullPath[len(fullPath)-1]
+
+	// Find file type
+	var extension string
+
+	// file is an executable
+	if !strings.Contains(abs, "home") {
+		extension = "executable"
+	} else { // file is in home dir
+		if strings.Contains(abs, ".") {
+			extension = strings.Split(abs, ".")[1]
+		} else {
+			extension = "none"
+		}
+	}
+	return fileName, extension
 }
